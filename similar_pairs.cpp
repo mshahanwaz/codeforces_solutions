9da1f635f2ec10a9216eb6a0ecb8db631c182aab
mshahanwaz <mohdshahanwaz55@gmail.com>
2022-01-27 19:20:26 +0530

    Added solutions

---
+++ similar_pairs.cpp
@@ -0,0 +1,106 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using str = string;
+using pi = pair<int, int>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vvi = vector<vi>;
+using vpi = vector<pi>;
+using vb = vector<bool>;
+
+#define endl '\n'
+#define fi first
+#define se second
+#define pb push_back
+#define pob pop_back
+#define pf push_front
+#define pof pop_front
+#define eb emplace_back
+#define ins insert
+#define mp make_pair
+#define mt make_tuple
+#define rev reverse
+#define precise(x) cout << fixed << setprecision(x)
+#define accum(x, y) accumulate(all(x), y)
+#define rep(x) for(int i = 0; i < x; i++)
+#define rep2(x, y) for(int i = x; i < y; i++)
+#define rep3(x, y, z) for(int i = x; j < y; i += z)
+#define per(x) for(int i = x; i >= 0; i--)
+#define per2(x, y) for(int i = x; i >= y; i--)
+#define per3(x, y, z) for(int i = x; i >= y; i -= z)
+#define repa(x, y) for(auto x : y)
+#define repr(x, y) for(auto &x : y)
+#define krep(i, x) for(int i = 0; i < x; i++)
+#define krep2(i, x, y) for(int i = x; i < y; i++)
+#define krep3(i, x, y, z) for(int i = x; i < y; i += z)
+#define debug(x) cout << #x << " = " << x << endl
+#define all(x) begin(x), end(x)
+#define rall(x) rbegin(x), rend(x)
+#define sz(x) (int)x.size()
+#define len(x) (int)x.length()
+
+const int dr4[] = {0, 1, 0, -1};
+const int dc4[] = {1, 0, -1, 0};
+
+const int dr8[] = {0, 1, 0, -1, 1, -1, -1, 1};
+const int dc8[] = {1, 0, -1, 0, -1, -1, 1, 1};
+
+void setIO(str name = "") {
+	cin.tie(0) -> sync_with_stdio(0);
+	if(sz(name)) {
+		freopen((name + ".in").c_str(), "r", stdin);
+		freopen((name + ".out").c_str(), "w", stdout);
+	}
+}
+
+template<typename T>
+bool ckmax(T &a, T b) { return b > a ? a = b, 1 : 0; };
+template<typename T>
+bool ckmin(T &a, T b) { return b < a ? a = b, 1 : 0; };
+
+/* Main Code */
+
+void solve() {
+	int n; cin >> n;
+	vi a(n);
+	int o = 0, e = 0;
+	rep(n) {
+		cin >> a[i];
+		if(a[i] & 1) ++o;
+		else ++e;
+	}
+	bool res = false;
+	bool fo = o & 1, fe = e & 1;
+	if(fo == 0 && fe == 0) res = true;
+	else if(fe == fo) {
+		vi odd, even;
+		sort(all(a));
+		rep(n) {
+			if(a[i] & 1) odd.pb(a[i]);
+			else even.pb(a[i]);
+		}
+		int po = 0, pe = 0;
+		bool flag = false;
+		while(po < sz(odd) && pe < sz(even)) {
+			int diff = odd[po] - even[pe];
+			if(diff == 1 || diff == -1) {
+				res = true;
+				break;
+			}
+			if(res) break;
+			if(diff < 0) ++po;
+			else ++pe;
+		}
+	}
+	cout << (res ? "YES" : "NO") << endl;
+}
+
+int main() {
+	setIO();
+	int tc = 1; cin >> tc;
+	while(tc--) solve();
+	return 0;
+}
+
