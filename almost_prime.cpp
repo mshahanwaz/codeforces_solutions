ea78a0a49bfe0ec85a749fd50bdea5a4a6a600b4
mshahanwaz <mohdshahanwaz55@gmail.com>
2022-02-11 17:27:38 +0530

    Modified solution

--- almost_prime.cpp
+++ almost_prime.cpp
@@ -5,28 +5,43 @@ const int N = 3005;
 
 void solve() {
 	int n; cin >> n;
-	vector<int> prime;
-	vector<bool> is_prime(N + 1, true);
+	// vector<int> prime;
+	// vector<bool> is_prime(N + 1, true);
+	// is_prime[0] = is_prime[1] = false;
+	// for(int i = 2; i <= N; i++) {
+	// 	if(is_prime[i]) {
+	// 		prime.push_back(i);
+	// 		for (int j = i * i; j <= N; j += i) {
+	// 			is_prime[j] = false;
+	// 		}
+	// 	}
+	// }
+	// int res = 0;
+	// for (int p = 6; p <= n; p++) {
+	// 	int cnt = 0;
+	// 	for (int i = 0; prime[i] <= p; i++) {
+	// 		if(p % prime[i] == 0) {
+	// 			cnt++;
+	// 			int tmp = p;
+	// 			while(tmp % prime[i] == 0) tmp /= prime[i];
+	// 		}
+	// 	}
+	// 	if(cnt == 2) res++;
+	// }
+	// cout << res << endl;
+	vector<bool> is_prime(n + 1, true);
 	is_prime[0] = is_prime[1] = false;
-	for(int i = 2; i <= N; i++) {
+	vector<int> factor(n + 1);
+	int res = 0;
+	for (int i = 2; i <= n; i++) {
 		if(is_prime[i]) {
-			prime.push_back(i);
-			for (int j = i * i; j <= N; j += i) {
+			for (int j = 2 * i; j <= n; j += i) {
+				++factor[j];	
 				is_prime[j] = false;
 			}
 		}
-	}
-	int res = 0;
-	for (int p = 6; p <= n; p++) {
-		int cnt = 0;
-		for (int i = 0; prime[i] <= p; i++) {
-			if(p % prime[i] == 0) {
-				cnt++;
-				int tmp = p;
-				while(tmp % prime[i] == 0) tmp /= prime[i];
-			}
-		}
-		if(cnt == 2) res++;
+		if(factor[i] == 2) 
+			res++;
 	}
 	cout << res << endl;
 }
