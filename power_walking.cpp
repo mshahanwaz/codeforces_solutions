0dce74e863eb0fbb9b8a9cf1952f3ad50833682b
mshahanwaz <mohdshahanwaz55@gmail.com>
2022-02-24 14:17:35 +0530

    Modified solution

--- power_walking.cpp
+++ power_walking.cpp
@@ -14,40 +14,14 @@ int main() {
 		int n;
 		cin >> n;
 		vector<int> a(n);
-		for (int &x: a)
+		set<int> aux;
+		for (int &x: a) {
 			cin >> x;
-		vector<pair<int, int>> aux;
-		sort(a.begin(), a.end());
-		for (int i = 0; i < n; ++i) {
-			int cnt = 0, j = i;
-			while (j < n && a[j] == a[i]) ++cnt, ++j;
-			aux.emplace_back(a[i], cnt);
-			i = j - 1;
+			aux.insert(x);
 		}
-		sort(aux.begin(), aux.end(), [&](auto p, auto q) {
-			if (p.second < q.second)
-				return (p.second < q.second);
-			return (p.first < p.first);
-		});
-		int m = (int)aux.size();
-		vector<int> res;
-		res.push_back(m);
-		int prev = m, i = 0;
-		int last = 0;
-		while (i < m) {
-			if (aux[i].second == 1) {
-				if (last == aux[i].first) 
-					++prev;
-				++i;
-			} else {
-				--aux[i].second;
-				if (!last || last == aux[i].first) ++prev;
-				last = aux[i].first;
-			}
-			if (i < m) res.push_back(prev);
-		}
-		for (int x: res)
-			cout << x << ' ';
+		int cnt = (int)aux.size();
+		for (int k = 1; k <= n; ++k)
+			cout << max(k, cnt) << ' ';
 		cout << endl;
 	}
 	return 0;
